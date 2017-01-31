14d324cbd7cb27502ccae60758b33404678bee33
noczero <zeroonetm@gmail.com>
2017-01-31 13:18:40 +0700

    First

---
+++ LatihanJava/src/latihanjava/Boleean.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanjava;
+
+/**
+ *
+ * @author zeroone
+ */
+public class Boleean {
+    public static void main(String[] args ){
+    boolean b = false;
+        b = true;
+        
+        boolean toBe = false;
+        b = toBe || !toBe;
+        if (b) {
+            System.out.println(toBe);
+        }
+        
+        int children = 0;
+       
+    }
+    
+}
---
+++ LatihanJava/src/latihanjava/Tipedata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihanjava;
+
+/**
+ *
+ * @author zeroone
+ */
+public class Tipedata {
+    public static void main(String[] args ){
+      // byte (number, 1 byte)
+      // short (number, 2 bytes)
+      // int (number, 4 bytes)
+      //long (number, 8 bytes)
+      //float (float number, 4 bytes)
+      //double (float number, 8 bytes)
+      //char (a character, 2 bytes)
+      // boolean (true or false, 1 byte)
+       
+        // Pake string 
+        String s1 = "Satrya Budi Pratama";
+        String s2 = " Belajar ";
+        String s3 = s1 + s2;
+        
+                
+                
+         
+        
+    }
+    
+    
+}
